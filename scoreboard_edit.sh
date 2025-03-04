d5771ae01457c37473193fbd50cbb19effe36846
XDturtleXD <69451855+XDturtleXD@users.noreply.github.com>
2025-03-04 10:56:43 +0800

    Bug Fixed

--- scoreboard_edit.sh
+++ scoreboard_edit.sh
@@ -1,10 +1,16 @@
-curl -X 'PATCH' "https://be.pdogs.ntu.im/team-contest-scoreboard/30" \
-    -H "auth-token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjo2ODA1LCJleHBpcmUiOiIyMDI1LTAzLTA5VDIyOjU2OjI1LjQ2NzUxMSIsImNhY2hlZF91c2VybmFtZSI6ImVyaWMyOTY5In0.fTmglPhvNGbDneZ-8HfzfoaC_sHZpQUESU7LpqiV9Oc" \
+read -p "Board ID: " board_id
+read -p "Auth Token: " auth_token
+read -a problem_id -p "Problem ID: "
+read -p "Penalty: " penalty
+list=$(printf "%s, " ${problem_id[@]})
+list='['${list::-2}']'
+curl -X 'PATCH' "https://be.pdogs.ntu.im/team-contest-scoreboard/""$board_id" \
+    -H "auth-token: ""$auth_token" \
     -H "Content-Type: application/json" \
     -d '{
-          "challenge_label": "X",
-          "title": "X",
-          "target_problem_ids": [1801, 1802, 1803, 1804],
-          "penalty_formula": "solved_time_mins * 1 + wrong_submissions * 50",
+          "challenge_label": "aaa",
+          "title": "aaa",
+          "target_problem_ids":'"$list"',
+          "penalty_formula": "solved_time_mins * 1 + wrong_submissions * '"$penalty"'",
           "team_label_filter": ""
         }'
