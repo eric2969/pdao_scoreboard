27a5c93977cb9f1a106722c9bea82354174ebf58
eric2969 <eric2969@gmail.com>
2025-04-06 15:33:49 +0800

    create set api

---
+++ be_set/set_lock.php
@@ -0,0 +1,16 @@
+<?php
+    $debug = false; // Set to true to enable debug mode
+    if($debug) {
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+    } else {
+        error_reporting(0);
+        ini_set('display_errors', 0);
+        ini_set('display_startup_errors', 0);
+    }
+    $token_file = fopen("../credit/Unlock-token.txt", "r") or die("Unable to open Auth token file!");
+    $token = trim(fgets($token_file));
+    fclose($token_file);
+    echo $token;
+?>
