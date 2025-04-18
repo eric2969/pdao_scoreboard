62398b57c9ee7abe5f679943d1b72286f5bc7f55
eric2969 <eric2969@gmail.com>
2025-04-18 20:49:37 +0800

    add stream mode

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,15 @@ define([
 ],
 function(Spotboard, $) {
     // 비동기로 데이터를 로드한 후 시작
+    const params = new URLSearchParams(window.location.search);
+    if(params.get('stream') == "on"){
+        $("#nyan-gif").remove();
+        $("#mlg-container").remove();
+        $("#snoopdogg-container").remove();
+        $("#oiiai-gif").remove();
+        $("#pop-gif").remove();
+        $("#quick-gif").remove();
+    }
     $.when(
         Spotboard.Manager.loadContest(),
         Spotboard.Manager.loadRuns()
